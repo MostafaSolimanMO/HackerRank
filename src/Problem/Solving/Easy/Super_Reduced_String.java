f71c23106e60d7d6cfe656d667451a05438b12c4
Mostafa Soliman <mostafamoma.moma@gmail.com>
2020-02-15 23:55:07 +0200

    Add challenge

---
+++ src/Problem/Solving/Easy/Super_Reduced_String.java
@@ -0,0 +1,19 @@
+package Problem.Solving.Easy;
+
+import java.util.Scanner;
+
+public class Super_Reduced_String {
+    // Complete the superReducedString function below.
+    static String superReducedString(String s) {
+
+        return "Empty String";
+    }
+
+    public static void main(String[] args) {
+
+        String s = new Scanner(System.in).nextLine();
+        String result = superReducedString(s);
+        System.out.println(result);
+
+    }
+}
