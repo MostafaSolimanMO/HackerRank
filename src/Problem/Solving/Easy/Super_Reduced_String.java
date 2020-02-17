ea67b1f728fa77c466e956c932e5a2753bde5605
Mostafa Soliman <mostafamoma.moma@gmail.com>
2020-02-17 08:12:54 +0200

    Solved some challenges

--- src/Problem/Solving/Easy/Super_Reduced_String.java
+++ src/Problem/Solving/Easy/Super_Reduced_String.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class Super_Reduced_String {
     // Complete the superReducedString function below.
     static String superReducedString(String s) {
-
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                s = s.substring(0, i - 1) + s.substring(i + 1);
+                i = 0;
+            }
+        }
+        if (!s.isEmpty()) return s;
         return "Empty String";
     }
 
