b56b28a3241a082a69b4a2bc58fb31acfffed155
mostafa <mostafamoma.moma@gmail.com>
2020-04-08 07:07:22 +0200

    Solved Day 17: More Exceptions challenge.

---
+++ src/DaysOfCode/Day17_MoreExceptions.java
@@ -0,0 +1,34 @@
+package DaysOfCode;
+
+import java.util.Scanner;
+
+public class Day17_MoreExceptions {
+    public void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+
+            int n = in.nextInt();
+            int p = in.nextInt();
+            Calculator myCalculator = new Calculator();
+            try {
+                int ans = myCalculator.power(n, p);
+                System.out.println(ans);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        in.close();
+    }
+
+    class Calculator {
+        public int power(int x, int y) throws Exception {
+            if (x < 0 || y < 0) {
+                throw new Exception("n and p should be non-negative");
+            } else {
+                return (int) Math.pow(x, y);
+            }
+        }
+    }
+}
